c6e86e3f71b04f1d2475a3fdfb02418beab43d9f
RamonHA13 <raan.heam@gmail.com>
2023-06-24 19:03:47 -0600

    Handle error status

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,54 +1,66 @@
-import React from "react";
+import React, { useState } from 'react'
 
-import { Text, View, TextInput, StyleSheet, TouchableOpacity, Dimensions } from "react-native";
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { COLORS, SIZES } from '../constants';
-import WavyHeader from "./../components/WavyHeader";
-import { useState } from "react";
-import axios from "axios";
-import { useNavigation } from "@react-navigation/native";
+import { Text, View, TextInput, StyleSheet, TouchableOpacity, Dimensions, Alert } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { COLORS, SIZES } from '../constants'
+import WavyHeader from './../components/WavyHeader'
 
-import Ioniocons from "react-native-vector-icons/Ionicons";
-import { config } from "./../config";
+import axios from 'axios'
+import { useNavigation } from '@react-navigation/native'
+
+import Ioniocons from 'react-native-vector-icons/Ionicons'
+import { config } from './../config'
 
 const RegisterScreen = () => {
-  const navigation = useNavigation();
-
+  const navigation = useNavigation()
 
   const userDefaultData = {
     nombreCompleto: '',
     correo: '',
     contraseña: '',
-    segundaContraseña: '',
+    segundaContraseña: ''
   }
 
   const [inputValue, setInputValue] = useState(userDefaultData)
 
   const handleRegister = (e) => {
-
-    if (validatePassword() && verfiedPasswordSecurity()) {
+    console.log('First here')
+    if (validatePassword()) {
       const { segundaContraseña, ...postUserData } = inputValue
       axios.post(config.apiUrl + '/api/auth/register', postUserData)
         .then(response => {
-          const responseParsered = response.data
+          const responseStatus = response.status
 
-          if ('token' in responseParsered) {
+          if (responseStatus === 201) {
+            console.log('here 201')
+            Alert.alert('¡Usuario registrado exitosamente!')
+            navigation.navigate('Login')
+          }
 
-            navigation.navigate('Login');
-          } else {
-            console.log('Aqui va un alert de que la respuesta es erronea');
+          if (responseStatus === 401) {
+            console.log('here 401')
+            Alert.alert('Error en el formato de la información')
           }
         })
-        .catch(error => console.error(error))
-      return
+        .catch(error => {
+          if (error.response.status === 409) {
+            console.log('here 409')
+            Alert.alert('Este correo electronico ya se encuentra registrado')
+          }
+          if (error.response.status === 500) {
+            Alert.alert('Error en el servidor intentelo mas tarde')
+          }
+        })
+    } else {
+      Alert.alert('Las contraseñas deben coincidir')
     }
   }
 
-  const verfiedPasswordSecurity= () => {
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+  // const verfiedPasswordSecurity = () => {
+  //   const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/
 
-   return regex.test(inputValue.contraseña);
-  }
+  //   return regex.test(inputValue.contraseña)
+  // }
 
   const validatePassword = () => {
     return inputValue.contraseña === inputValue.segundaContraseña
@@ -61,12 +73,12 @@ const RegisterScreen = () => {
           customStyles={styles.svgCurve}
           customHeight={90}
           customTop={80}
-          customBgColor={"#CE5959"}
-          customWavePattern={"M0,32L18.5,48C36.9,64,74,96,111,90.7C147.7,85,185,43,222,64C258.5,85,295,171,332,224C369.2,277,406,299,443,288C480,277,517,235,554,192C590.8,149,628,107,665,96C701.5,85,738,107,775,133.3C812.3,160,849,192,886,192C923.1,192,960,160,997,133.3C1033.8,107,1071,85,1108,112C1144.6,139,1182,213,1218,229.3C1255.4,245,1292,203,1329,186.7C1366.2,171,1403,181,1422,186.7L1440,192L1440,0L1421.5,0C1403.1,0,1366,0,1329,0C1292.3,0,1255,0,1218,0C1181.5,0,1145,0,1108,0C1070.8,0,1034,0,997,0C960,0,923,0,886,0C849.2,0,812,0,775,0C738.5,0,702,0,665,0C627.7,0,591,0,554,0C516.9,0,480,0,443,0C406.2,0,369,0,332,0C295.4,0,258,0,222,0C184.6,0,148,0,111,0C73.8,0,37,0,18,0L0,0Z"}
+          customBgColor={'#CE5959'}
+          customWavePattern={'M0,32L18.5,48C36.9,64,74,96,111,90.7C147.7,85,185,43,222,64C258.5,85,295,171,332,224C369.2,277,406,299,443,288C480,277,517,235,554,192C590.8,149,628,107,665,96C701.5,85,738,107,775,133.3C812.3,160,849,192,886,192C923.1,192,960,160,997,133.3C1033.8,107,1071,85,1108,112C1144.6,139,1182,213,1218,229.3C1255.4,245,1292,203,1329,186.7C1366.2,171,1403,181,1422,186.7L1440,192L1440,0L1421.5,0C1403.1,0,1366,0,1329,0C1292.3,0,1255,0,1218,0C1181.5,0,1145,0,1108,0C1070.8,0,1034,0,997,0C960,0,923,0,886,0C849.2,0,812,0,775,0C738.5,0,702,0,665,0C627.7,0,591,0,554,0C516.9,0,480,0,443,0C406.2,0,369,0,332,0C295.4,0,258,0,222,0C184.6,0,148,0,111,0C73.8,0,37,0,18,0L0,0Z'}
         />
         <View style={styles.headerContainer}>
           <TouchableOpacity onPress={() => {
-            navigation.navigate('Login');
+            navigation.navigate('Login')
           }}>
             <Ioniocons name='arrow-back' size={25} color={'white'} />
           </TouchableOpacity>
@@ -125,7 +137,7 @@ const RegisterScreen = () => {
           <Text style={{
             color: COLORS.gray,
             fontSize: SIZES.large,
-            marginTop: 30,
+            marginTop: 30
           }}>CucStore</Text>
         </View>
       </KeyboardAwareScrollView>
@@ -137,13 +149,13 @@ const RegisterScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f1f1f1',
+    backgroundColor: '#f1f1f1'
   },
   headerContainer: {
     marginTop: 50,
     marginHorizontal: 10,
     alignItems: 'center',
-    flexDirection: 'row',
+    flexDirection: 'row'
   },
   headerText: {
     fontSize: SIZES.xLarge,
@@ -151,7 +163,7 @@ const styles = StyleSheet.create({
     color: COLORS.white,
     textAlign: 'center',
     marginLeft: 'auto',
-    marginRight: 'auto',
+    marginRight: 'auto'
   },
   svgCurve: {
     position: 'absolute',
@@ -160,7 +172,7 @@ const styles = StyleSheet.create({
   viewContainer: {
     paddingTop: 100,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   textInput: {
     padding: 10,
@@ -169,13 +181,13 @@ const styles = StyleSheet.create({
     height: 50,
     marginTop: 20,
     borderRadius: 10,
-    backgroundColor: '#FFDD83',
+    backgroundColor: '#FFDD83'
   },
   subText: {
     color: COLORS.gray,
     fontSize: SIZES.small,
     marginTop: 10,
-    marginBottom: 10,
+    marginBottom: 10
   },
   button: {
     backgroundColor: COLORS.primary,
@@ -185,17 +197,17 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     alignItems: 'center',
     justifyContent: 'center',
-    shadowRadius: 10,
+    shadowRadius: 10
   },
   buttonText: {
     fontSize: SIZES.medium,
-    color: COLORS.white,
+    color: COLORS.white
   },
   logo: {
     marginBottom: 40,
     width: '80%',
-    height: 100,
-  },
-});
+    height: 100
+  }
+})
 
-export default RegisterScreen;
+export default RegisterScreen
