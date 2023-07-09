e57aff3f1f507c8a4e93cc5cd33e3cc79a7a99da
RamonHA13 <raan.heam@gmail.com>
2023-07-08 20:32:29 -0600

    Add socket services

---
+++ src/utils/socket.js
@@ -0,0 +1,56 @@
+import { io } from 'socket.io-client'
+import { config } from '../config'
+
+/* eslint-disable no-var */
+var socket
+var userId
+export function connectSocket (Id) {
+  userId = Id
+  socket = io(`${config.apiUrl}`, {
+    extraHeaders: {
+      user: Id
+    }
+  })
+}
+
+export function disconnectSocket () {
+  socket.disconnect()
+}
+
+export function getUserChats () {
+  const userChats = []
+  socket.emit('getUserChats')
+  socket.on('chatRooms', (chats) => {
+    if (chats) {
+      chats.forEach(chat => {
+        const from = chat.participantes.find(participante => participante.id === userId)
+        const to = chat.participantes.find(participante => participante.id !== userId)
+
+        userChats.push({
+          id: chat.id,
+          createdAt: chat.createdAt,
+          messages: chat.messages,
+          from: {
+            id: from.id,
+            imgUrl: from.imgUrl,
+            nombreCompleto: from.nombreCompleto,
+            nombreMarca: from.nombreMarca || null,
+            chats: from.chats
+          },
+          to: {
+            id: to.id,
+            imgUrl: to.imgUrl,
+            nombreCompleto: to.nombreCompleto,
+            nombreMarca: to.nombreMarca || null,
+            chats: to.chats
+          }
+        })
+      })
+    }
+  })
+  return userChats
+}
+
+export default function joinRoom (room) {
+  socket.emit('joinRoom', room)
+}
