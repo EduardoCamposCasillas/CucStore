bdd8a5d12e225077a24e03cf91c194dd1f8ea8ba
RamonHA13 <raan.heam@gmail.com>
2023-07-10 13:33:03 -0600

    Refactor socket utils

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,8 +2,9 @@ import { io } from 'socket.io-client'
 import { config } from './../config'
 
 /* eslint-disable no-var */
-var socket
+export var socket
 var userId
+
 export function connectSocket (Id) {
   userId = Id
   socket = io(`${config.apiUrl}`, {
@@ -17,58 +18,29 @@ export function disconnectSocket () {
   socket.disconnect()
 }
 
-export function getUserChats () {
-  const userChats = []
-  socket.emit('getUserChats')
-  socket.on('chatRooms', (chats) => {
-    if (chats) {
-      chats.forEach(chat => {
-        const from = chat.participantes.find(participante => participante.id === userId)
-        const to = chat.participantes.find(participante => participante.id !== userId)
+export function createUserChat (to) {
+  socket.emit('createUserChat', { usuario: userId, to })
+}
 
-        userChats.push({
-          id: chat.id,
-          createdAt: chat.createdAt,
-          messages: chat.messages,
-          from: {
-            id: from.id,
-            imgUrl: from.imgUrl,
-            nombreCompleto: from.nombreCompleto,
-            nombreMarca: from.nombreMarca || null,
-            chats: from.chats
-          },
-          to: {
-            id: to.id,
-            imgUrl: to.imgUrl,
-            nombreCompleto: to.nombreCompleto,
-            nombreMarca: to.nombreMarca || null,
-            chats: to.chats
-          }
-        })
-      })
+export const parseChat = ({ chat, from, to }) => {
+  const chatParsered = {
+    id: chat.id,
+    createdAt: chat.createdAt,
+    messages: chat.messages,
+    from: {
+      id: from.id,
+      imgUrl: from.imgUrl,
+      nombreCompleto: from.nombreCompleto,
+      nombreMarca: from.nombreMarca || null,
+      chats: from.chats
+    },
+    to: {
+      id: to.id,
+      imgUrl: to.imgUrl,
+      nombreCompleto: to.nombreCompleto,
+      nombreMarca: to.nombreMarca || null,
+      chats: to.chats
     }
-  })
-  return userChats
-}
-
-export function joinRoom (room) {
-  socket.emit('joinRoom', room)
-}
-
-export function getMessages (room) {
-  const messages = []
-  socket.emit('getMessages', room)
-  socket.on('messages', (messagesData) => {
-    messagesData.forEach(message => {
-      messages.push(message)
-    })
-  })
-  socket.on('message', (message) => {
-    console.log(message)
-    messages.unshift(message)
-  })
-  return messages
-}
-export function sendMessages (message) {
-  socket.emit('message', message)
+  }
+  return chatParsered
 }
